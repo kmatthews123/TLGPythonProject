36d53cb3c7f66e1f64672dbf5a7834dfb6cda176
kmatthews123 <kmatt999@gmail.com>
2024-07-10 11:26:30 -0700

    fixed readablitiy of output code.py file

--- main.py
+++ main.py
@@ -21,65 +21,63 @@ def createCode(axvarcode, buttonvarcode, hatsvarcode):
             codenew.write(line)
 
         codenew.write("js = Joystick()\n")
-        inputs = ""
+        #inputs = ""
+        codenew.write("js.add_input( \n")
 
         #simple axes input of analog pins
         #formated as such: Axis(board.A2),
         for i in range(axvarcode):
-            analoginput = "Axes.board(" + input(f"please enter analog pin for axis {i}: ") + "), "
-            inputs += analoginput
+            analoginput = "    Axis(board.A" + input(f"please enter analog pin number for axis {i}: ") + "),\n"
+            codenew.write(analoginput)
 
         #simple button input for digital pins
         # formated as such: Button(board.D10),
-
         for i in range(buttonvarcode):
-            buttoninput = "Button.board(" + input(f"please enter Digital pin for button {i}: ") + "), "
-            inputs += buttoninput
+            buttoninput = "    Button(board.D" + input(f"please enter Digital pin number for button {i}: ") + "),\n"
+            codenew.write(buttoninput)
 
         #Hat input, same as setting up 4 buttons
         #formated as such: Hat(up=board.D2, down=board.D3, left=board.D4, right=board.D7)
+        #will need to setup count for this one and remove trailing "," for last line
 
         for i in range(hatsvarcode):
-            #initalize string for hat items
-            hatinput = "Hat("
-            #pulls in inputs for hat directions
-            hatup = "up=board." + input("please enter Digital pin for up direction: ") + ", "
-            hatdown = "down=board." + input("please enter Digital pin for the down direction: ") + ", "
-            hatleft = "left=board." + input("please enter Digital pin for left directon: ") + ", "
-            hatright = "right=board." + input("please enter Digital pin for the right direction: ") + "), "
+                #print(i)
+                #print(hatsvarcode)
+                #this loop itterates though the nubmer of hats and since it is the last set of things to be decared it will write the last item without the trailing "," to finish the tuple
+                if i != hatsvarcode -1 :
+                    #initalize string for hat items
+                    hatinput = "    Hat("
+                    #pulls in inputs for hat directions
+                    hatup = "up=board.D" + input("please enter Digital pin for up direction: ") + ", "
+                    hatdown = "down=board.D" + input("please enter Digital pin for the down direction: ") + ", "
+                    hatleft = "left=board.D" + input("please enter Digital pin for left directon: ") + ", "
+                    hatright = "right=board.D" + input("please enter Digital pin for the right direction: ") + "),\n"
+                    hatinput += hatup + hatdown + hatleft + hatright
+                    # codenew.write(hatinput)
+                else:
+                    #initalize string for hat items
+                    hatinput = "    Hat("
+                    #pulls in inputs for hat directions
+                    hatup = "up=board.D" + input("please enter Digital pin for up direction: ") + ", "
+                    hatdown = "down=board.D" + input("please enter Digital pin for the down direction: ") + ", "
+                    hatleft = "left=board.D" + input("please enter Digital pin for left directon: ") + ", "
+                    hatright = "right=board.D" + input("please enter Digital pin for the right direction: ") + ")\n"
+                    hatinput += hatup + hatdown + hatleft + hatright
+                    # codenew.write(hatinput)
+                codenew.write(hatinput)
+
+
             
-            hatinput += hatup + hatdown + hatleft + hatright
+        #print(inputs)
 
-            inputs += hatinput
         
-        print(inputs)
-
-        codenew.write("js.add_input(")
-        codenew.write(inputs[:-2])
-        codenew.write(")")
+        #codenew.write(inputs[:-2])
+        codenew.write(")\n")
+        codenew.write("while True:\n")
+        codenew.write("    js.update()\n")
 
         print(axvarcode, buttonvarcode, hatsvarcode)
 
-
-        '''
-
-        #need to append the below to output/code.py
-
-        js = Joystick()
-
-        js.add_input(
-            Button(board.D9),
-            Button(board.D10),
-            Axis(board.A2),
-            Axis(board.A3),
-            Hat(up=board.D2, down=board.D3, left=board.D4, right=board.D7)
-            #no comma after last item
-        )
-
-        while True:
-            js.update()
-                
-        '''
 def main():
     #used to overwrite boot.py if it already exists
     while os.path.exists("output/boot.py"):
